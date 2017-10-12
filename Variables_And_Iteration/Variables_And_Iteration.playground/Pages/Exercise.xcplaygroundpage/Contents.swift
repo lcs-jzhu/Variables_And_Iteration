e90b2374e89711f28964290d42aad3d05e12b3e5
Jessie Zhu <jezhu@lcs-mba3260.lcs.on.ca>
2017-10-12 16:10:01 -0400

    Did the mountains

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -16,23 +16,59 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
+canvas.drawShapesWithBorders = false
 
-canvas.drawRectangle(centreX: 150, centreY: 50, width: 300, height: 100)
-canvas.drawLine(fromX: 0, fromY: 130, toX: 30, toY: 100)
-canvas.drawLine(fromX: 30, fromY: 100, toX: 60, toY: 130)
-canvas.drawLine(fromX: 60, fromY: 130, toX: 90, toY: 100)
-canvas.drawLine(fromX: 90, fromY: 100, toX: 120, toY: 130)
-canvas.drawLine(fromX: 120, fromY: 130, toX: 150, toY: 100)
-canvas.drawLine(fromX: 150, fromY: 100, toX: 180, toY: 130)
-canvas.drawLine(fromX: 180, fromY: 130, toX: 210, toY: 100)
-canvas.drawLine(fromX: 210, fromY: 100, toX: 240, toY: 130)
-canvas.drawLine(fromX: 240, fromY: 130, toX: 270, toY: 100)
-canvas.drawLine(fromX: 270, fromY: 100, toX: 300, toY: 130)
-canvas.drawLine(fromX: 0, fromY: 250, toX: 60, toY: 200)
-canvas.drawLine(fromX: 60, fromY: 200, toX: 120, toY: 250)
-canvas.drawLine(fromX: 120, fromY: 250, toX: 180, toY: 200)
-canvas.drawLine(fromX: 180, fromY: 200, toX: 240, toY: 250)
-canvas.drawLine(fromX: 240, fromY: 250, toX: 300, toY: 200)
+//the dots
+canvas.defaultBorderWidth = 1
+for x in stride(from: 0, through: 300, by: 25)
+{
+    for y in stride(from: 300, through: 0, by: -25)
+    {
+        canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
+    }
+}
+
+canvas.translate(byX: 0, byY: 150)
+canvas.fillColor = Color.init(hue: 0, saturation: 0, brightness: 70, alpha: 100)
+for _ in 1...4
+{
+    canvas.rotate(by: 45)
+    canvas.drawRectangle(centreX: 0, centreY: 0, width: 100, height: 100)
+    canvas.rotate(by: -45)
+    canvas.translate(byX: 100, byY: 0)
+}
+canvas.translate(byX: -400, byY: -150)
+canvas.translate(byX: 150, byY: 75)
+canvas.translate(byX: -150, byY: -75)
+canvas.translate(byX: 150, byY: 75)
+canvas.drawRectangle(centreX: 0, centreY: 0, width: 300, height: 150)
+canvas.translate(byX: -150, byY: 0)
+canvas.fillColor = Color.init(hue: 0, saturation: 0, brightness: 50, alpha: 100)
+for _ in 1...8
+{
+    canvas.rotate(by: 45)
+    canvas.drawRectangle(centreX: 0, centreY: 0, width: 50, height: 50)
+    canvas.rotate(by: -45)
+    canvas.translate(byX: 50, byY: 0)
+}
+canvas.translate(byX: -400, byY: -75)
+canvas.translate(byX: 150, byY: 0)
+canvas.drawRectangle(centreX: 0, centreY: 0, width: 300, height: 150)
+
+canvas.lineColor = Color.init(hue: 0, saturation: 0, brightness: 70, alpha: 20)
+//the mountains
+
+PlaygroundPage.current.liveView = canvas.imageView
+
+
+
+
+
+
+
+
+
+
 
 
 
