6b54ea83346d2d5777f488d3612be8ade58effca
Jessie Zhu <jezhu@lcs-mba3260.lcs.on.ca>
2017-10-12 14:07:19 -0400

    complete the basic borders but not the fill colour.

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -16,12 +16,30 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
-//: ## Add your code below
+
+canvas.drawRectangle(centreX: 150, centreY: 50, width: 300, height: 100)
+canvas.drawLine(fromX: 0, fromY: 130, toX: 30, toY: 100)
+canvas.drawLine(fromX: 30, fromY: 100, toX: 60, toY: 130)
+canvas.drawLine(fromX: 60, fromY: 130, toX: 90, toY: 100)
+canvas.drawLine(fromX: 90, fromY: 100, toX: 120, toY: 130)
+canvas.drawLine(fromX: 120, fromY: 130, toX: 150, toY: 100)
+canvas.drawLine(fromX: 150, fromY: 100, toX: 180, toY: 130)
+canvas.drawLine(fromX: 180, fromY: 130, toX: 210, toY: 100)
+canvas.drawLine(fromX: 210, fromY: 100, toX: 240, toY: 130)
+canvas.drawLine(fromX: 240, fromY: 130, toX: 270, toY: 100)
+canvas.drawLine(fromX: 270, fromY: 100, toX: 300, toY: 130)
+canvas.drawLine(fromX: 0, fromY: 250, toX: 60, toY: 200)
+canvas.drawLine(fromX: 60, fromY: 200, toX: 120, toY: 250)
+canvas.drawLine(fromX: 120, fromY: 250, toX: 180, toY: 200)
+canvas.drawLine(fromX: 180, fromY: 200, toX: 240, toY: 250)
+canvas.drawLine(fromX: 240, fromY: 250, toX: 300, toY: 200)
 
 
 
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
- */
-PlaygroundPage.current.liveView = canvas.imageView
+
+
+
+
+
+
+
