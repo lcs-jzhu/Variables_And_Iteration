edb5e574904b36b970cdd17317c9bf95f9df0ac9
Jessie Zhu <jezhu@lcs-mba3260.local>
2017-10-18 12:07:00 -0400

    Done the lines firstly.

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
@@ -18,12 +18,96 @@ import PlaygroundSupport
 //: ## Add your code below
 
 // Create canvas
-let canvas = Canvas(width: 300, height: 300)
+let canvas = Canvas(width: 500, height: 500)
 
-// Below this line, try combining a loop and four statements that draw lines to generate the goal
+//// No fill
+//canvas.drawShapesWithFill = false
+//
+//// Below this line, try combining a loop and four statements that draw lines to generate the goal
+//// Loop to set horizontal position
+//for x in stride(from: 50, through: 450, by: 100){
+//
+//    // Loop tp set vertical position
+//    for y in stride(from: 450, through: 50, by: -100){
+//
+//         //Draw five squares
+//        for size in stride(from: 100, through: 20, by: -20)  {
+//
+//        canvas.drawRectangle(centreX: x, centreY: y, width: size, height: size)
+//        }
+//    }
+//
+//
+//
+//
+//}
+canvas.fillColor = Color.white
+canvas.borderColor = Color.black
+canvas.drawEllipse(centreX: 0, centreY: 250, width: 500, height: 500)
+canvas.drawEllipse(centreX: 500, centreY: 250, width: 500, height: 500)
+
+canvas.drawLine(fromX: 0, fromY: 500, toX: 150, toY: 350, lineWidth: 3)
+canvas.drawLine(fromX: 350, fromY: 350, toX: 500, toY: 500, lineWidth: 3)
+canvas.drawLine(fromX: 0, fromY: 0, toX: 150, toY: 150, lineWidth: 3)
+canvas.drawLine(fromX: 350, fromY: 150, toX: 500, toY: 0, lineWidth: 3)
+canvas.drawLine(fromX: 150, fromY: 350, toX: 350, toY: 350, lineWidth: 3)
+canvas.drawLine(fromX: 150, fromY: 350, toX: 150, toY: 150, lineWidth: 3)
+canvas.drawLine(fromX: 350, fromY: 150, toX: 500, toY: 0, lineWidth: 3)
+canvas.drawLine(fromX: 150, fromY: 150, toX: 350, toY: 150, lineWidth: 3)
+canvas.drawLine(fromX: 350, fromY: 350, toX: 350, toY: 150, lineWidth: 3)
+canvas.drawLine(fromX: 0, fromY: 450, toX: 150, toY: 350, lineWidth: 3)
+canvas.drawLine(fromX: 50, fromY: 500, toX: 150, toY: 350, lineWidth: 3)
+canvas.drawLine(fromX: 0, fromY: 50, toX: 150, toY: 150, lineWidth: 3)
+canvas.drawLine(fromX: 50, fromY: 0, toX: 150, toY: 150, lineWidth: 3)
+canvas.drawLine(fromX: 450, fromY: 0, toX: 350, toY: 150, lineWidth: 3)
+canvas.drawLine(fromX: 350, fromY: 150, toX: 500, toY: 50,lineWidth: 3)
+canvas.drawLine(fromX: 450, fromY: 500, toX: 350, toY: 350, lineWidth: 3)
+canvas.drawLine(fromX: 350, fromY: 350, toX: 500, toY: 450, lineWidth: 3)
+canvas.drawLine(fromX: 150, fromY: 250, toX: 250, toY: 350, lineWidth: 3)
+canvas.drawLine(fromX: 250, fromY: 350, toX: 350, toY: 250, lineWidth: 3)
+canvas.drawLine(fromX: 350, fromY: 250, toX: 250, toY: 150, lineWidth: 3)
+canvas.drawLine(fromX: 150, fromY: 250, toX: 250, toY: 150, lineWidth: 3)
+canvas.drawLine(fromX: 200, fromY: 300, toX: 200, toY: 200, lineWidth: 3)
+canvas.drawLine(fromX: 200, fromY: 300, toX: 300, toY: 300, lineWidth: 3)
+canvas.drawLine(fromX: 300, fromY: 300, toX: 300, toY: 200, lineWidth: 3)
+canvas.drawLine(fromX: 300, fromY: 200, toX: 200, toY: 200, lineWidth: 3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
  */
 PlaygroundPage.current.liveView = canvas.imageView
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
